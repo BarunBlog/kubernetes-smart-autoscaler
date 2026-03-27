4dd89a1f50f4af9bbbabbecbeb43723d333a91b7
BarunBlog <bhattacharjeebarun25@gmail.com>
2026-03-27 13:51:03 +0600

    fix(infra/worker): allowed the worker nodes to label it custom worker-type=worker

--- infra/k3s-cluster/worker/scripts/join_cluster.sh
+++ infra/k3s-cluster/worker/scripts/join_cluster.sh
@@ -36,8 +36,4 @@ echo "Joining cluster..."
 curl -sfL https://get.k3s.io | \
   K3S_URL=https://${MASTER_IP}:6443 \
   K3S_TOKEN=${K3S_TOKEN} \
-  sh -s - agent \
-  --node-label "node-role.kubernetes.io/worker=worker" \
-  --node-label "worker=true" \
-  --kubelet-arg="allowed-unsafe-sysctls=*" \
-  --kubelet-arg="node-labels=node-role.kubernetes.io/worker=worker,worker=true"
+  sh -s - agent --node-label "worker-type=worker"
